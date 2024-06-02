188305606951e7f7796a79ecf49b0053b97509d7
Steven Roh <hello@stevenroh.ch>
2024-06-02 16:40:57 +0200

    Init demo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wp-meetup-sion-demo
+
+https://playground.wordpress.net/?blueprint-url=https://raw.githubusercontent.com/stevenroh/wp-meetup-sion-demo/main/demo-blueprint.json
---
+++ demo-plugin/demo-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Plugin Name: Demo Plugin
+* Plugin URI:
+* Description: Nothing, this is just a demo
+* Version: 1.0
+* Author: Steven Roh
+* Author URI: https://stevenroh.ch
+* Text Domain: demo-plugin
+*/
